5e1732c0e73140910953340822d70b9c55b01174
Jan Kremlacek <36951+jankremlacek@users.noreply.github.com>
2022-08-08 21:47:29 +0200

    🔧

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-
+        chunkSizeWarningLimit: 1024,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    web3: ['ethers', 'wagmi']
+                },
+            },
+        },
     },
     define: {
         global: 'globalThis',
