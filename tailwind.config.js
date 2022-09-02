78bb7767ee09e9e414206fbfdddb0fe1e35c68f9
Jan Kremlacek <36951+jankremlacek@users.noreply.github.com>
2022-09-02 19:43:36 +0200

    Vite build as the future production
    To allow to setup Vercel CI the sam way

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,10 @@
 
 module.exports = {
     content: [
-        './public/**/*.{html,js}',
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
-		extend: {
-			// ...
-		},
-    },
-    plugins: [],
+        extend: {},
+    }
 }
