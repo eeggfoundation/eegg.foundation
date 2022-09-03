27a2b9ece184fe989a9119ac7f058491e67ac6c7
Jan Kremlacek <36951+jankremlacek@users.noreply.github.com>
2022-09-03 21:25:29 +0200

    Migrated to `nextjs`

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Head, Html, Main, NextScript } from 'next/document'
+
+export default function Document() {
+    return (
+        <Html className="antialiased" lang="en">
+            <Head>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+                <meta name="msapplication-TileColor" content="#ffffff" />
+                <meta name="theme-color" content="#ffffff" />
+            </Head>
+            <body className="flex h-full flex-col">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
+}
