a96b39620b35c8f5e64e222b90d506a65af0919b
Jan Kremlacek <36951+jankremlacek@users.noreply.github.com>
2022-09-05 12:16:59 +0200

    💅🏻

--- src/components/index/Hero.tsx
+++ src/components/index/Hero.tsx
@@ -90,7 +90,7 @@ export function Hero() {
                             and digital technology by creating a&nbsp;new form of ownership distributed to artists, galleries,
                             and collectors.
                         </h1>
-                        <p className="mt-6 text-xl text-gray-600">
+                        <p className="mt-6 text-xl text-gray-600 lg:leading-normal">
                             The EEGG (Electronic Egg) is a&nbsp;redefined art object. It consists of two indivisible parts.
                             One is a&nbsp;real work of Art, ROR (Real Object of Reality), and the other is its unique digital
                             version NFT (Non-Fungible Token). Both versions of the work are tradable only at the same time.
@@ -98,7 +98,7 @@ export function Hero() {
                     </div>
                     <div className="relative mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0">
                         <BackgroundIllustration className="absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0" />
-                        <div className="-mx-4 h-[448px] px-9 sm:mx-0 lg:absolute lg:-inset-x-10 lg:-top-14 lg:-bottom-20 lg:h-auto lg:px-0 lg:pt-10">
+                        <div className="-mx-4 h-[448px] px-9 sm:mx-0 lg:absolute lg:-inset-x-10 lg:-top-8 lg:-bottom-20 lg:h-auto lg:px-0 lg:pt-10">
                             <Image src={imgMonaLisaTriangluar} className="mx-auto max-w-[448px] drop-shadow-xl" alt="The Art" priority />
                         </div>
                     </div>
